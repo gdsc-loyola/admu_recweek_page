45de50fab93839df5beb1f030f3eb7373ce49636
Renzo Tan <renzotimtan@gmail.com>
2020-09-04 12:45:04 +0800

    added underlay effect

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,43 +5,75 @@ gsap.from(".gadgets-header", {
   ease: "power2.outs",
 });
 
-
 let gadgets = gsap.timeline({
   scrollTrigger: {
     trigger: ".gadgets",
-    start: "center center",
+    start: "56% center",
     toggleActions: "restart pause reverse pause",
     pin: true,
-    markers: true,
-    scrub: true,
+    scrub: 1,
   },
 });
 
 gadgets
-  .to(".laptop", { x: "-100%", opacity: 0 }, "<.5")
+  .to(".laptop", { x: "-100%", opacity: 0 })
   .to(".ipad", { x: "100%", opacity: 0 }, "<0")
-  .to(".gadgets", { opacity: 0, ease: "power4.outs" });
+  .to(".gadgets", { opacity: 0, ease: "power4.outs" })
 
 let community = gsap.timeline({
   scrollTrigger: {
     trigger: ".community",
-    start: "bottom bottom",
-    end: "bottom center",
-    toggleActions: "restart pause reverse pause",
+    start: "center center",
+    end: "bottom top",
     pin: true,
-    scrub: true,
-  },
-});
+    scrub: 1,
+    markers: true,
+  }
+})
 
-gsap.from(".community", {
+community
+  .to(".theme", { opacity: 0 })
+  .from(".community-body", { duration: 100, opacity: 0, delay: 100 })
+  .from(".earth", { scale: 0, duration: 100 })
+  .to(".community", { yPercent: -5, duration: 100 })
+
+
+
+let theme = gsap.timeline({
   scrollTrigger: {
-    trigger: ".community",
-    start: "bottom bottom",
-    end: "bottom center",
-    toggleActions: "restart pause reverse pause",
+    trigger: ".theme",
+    toggleActions: "pause",
     pin: true,
     scrub: true,
-  },
-  opacity: 0,
-});
+  }
+})
+
+theme
+  .to(".theme", { opacity: 1 })
+  .to(".theme img", { scale: 0.2 })
+
+
+let vision = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".vision",
+    start: "top top",
+    end: "bottom top",
+    pin: true,
+    scrub: true,
+  }
+})
+// let mission = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".mission",
+//     start: "top top",
+//     end: "bottom top",
+//     pin: true,
+//     scrub: true,
+//   }
+// })
+
+
+
+
+
 
