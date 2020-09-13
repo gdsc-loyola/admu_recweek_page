430fc470dc2c63c0612d31ff5ad49e18ba5c7503
Renzo Tan <renzotimtan@gmail.com>
2020-09-13 16:18:55 +0800

    finished animations in project sections

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,10 @@
+//  REGISTER PLUGIN
+
 gsap.registerPlugin(ScrollTrigger)
+
+//////////////////////////////////////////////////////
+// SECTION 1
+//////////////////////////////////////////////////////
 
 gsap.from(".gadgets-header", {
   duration: 1.5,
@@ -33,8 +39,8 @@ let community = gsap.timeline({
 })
 
 community
-  .from(".community", { duration: 100, opacity: 0, delay: 100 })
-  .from(".earth", { scale: 0, duration: 100 })
+  .from(".community", { duration: 1, opacity: 0, delay: 1 })
+  .from(".earth", { scale: 0, duration: 1 })
 
 let theme = gsap.timeline({
   scrollTrigger: {
@@ -46,7 +52,7 @@ let theme = gsap.timeline({
 })
 
 theme
-  .to(".theme", { opacity: 1, duration: 100 })
+  .to(".theme", { opacity: 1 })
   .to(".theme", { opacity: 0 }, "<4")
 
 
@@ -59,11 +65,11 @@ let vision = gsap.timeline({
 })
 
 vision
-  .from(".vision", { opacity: 0, duration: 50 })
-  .from(".vision-temporary", { x: "-150%", ease: "slow", duration: 50 })
-  .from(".vision-body", { opacity: 0, delay: 1, duration: 100 })
-  .to('.vision-body', { opacity: 0, duration: 50 })
-  .to(".vision-temporary", { x: "-150%", ease: "slow", duration: 50, opacity: 0 })
+  .from(".vision", { opacity: 0, duration: 1 })
+  .from(".vision-temporary", { x: "-150%", ease: "slow", duration: 1 })
+  .from(".vision-body", { opacity: 0, delay: 1, duration: 2 })
+  .to('.vision-body', { opacity: 0, duration: 1 })
+  .to(".vision-temporary", { x: "-150%", ease: "slow", duration: 1, opacity: 0 })
 
 
 let mission = gsap.timeline({
@@ -76,21 +82,24 @@ let mission = gsap.timeline({
 })
 
 mission
-  .to(".mission", { opacity: 1, ease: "slow", duration: 100 })
-  .from(".mission-temporary", { x: '-150%', ease: "slow", duration: 100 })
-  .from(".mission-1", { y: "80vh", ease: "slow", duration: 200, opacity: 0 })
+  .to(".mission", { opacity: 1, ease: "slow", duration: 1 })
+  .from(".mission-temporary", { x: '-150%', ease: "slow", duration: 1 })
+  .from(".mission-1", { y: "80vh", ease: "slow", duration: 2, opacity: 0 })
 
-  .to(".mission-1", { opacity: 0.3, ease: "slow", delay: 50, duration: 100 })
-  .from(".mission-2", { y: "80vh", ease: "slow", duration: 200, opacity: 0 })
-  .to(".mission-2", { opacity: 0.3, ease: "slow", delay: 50, duration: 100 })
-  .from(".mission-3", { y: "80vh", ease: "slow", duration: 200, opacity: 0 })
-  .to(".mission-3", { opacity: 0.3, ease: "slow", delay: 50, duration: 100 })
+  .to(".mission-1", { opacity: 0.3, ease: "slow", delay: 1, duration: 1 })
+  .from(".mission-2", { y: "80vh", ease: "slow", duration: 2, opacity: 0 })
+  .to(".mission-2", { opacity: 0.3, ease: "slow", delay: 1, duration: 1 })
+  .from(".mission-3", { y: "80vh", ease: "slow", duration: 2, opacity: 0 })
+  .to(".mission-3", { opacity: 0.3, ease: "slow", delay: 1, duration: 1 })
 
-  .to(".mission-1", { opacity: 1, ease: "slow", duration: 200 })
-  .to(".mission-2", { opacity: 1, ease: "slow", duration: 200 }, "<0")
-  .to(".mission-3", { opacity: 1, ease: "slow", duration: 200 }, "<0")
-  .to(".mission", { opacity: 0, ease: "slow", duration: 200 })
+  .to(".mission-1", { opacity: 1, ease: "slow", duration: 2 })
+  .to(".mission-2", { opacity: 1, ease: "slow", duration: 2 }, "<0")
+  .to(".mission-3", { opacity: 1, ease: "slow", duration: 2 }, "<0")
+  .to(".mission", { opacity: 0, ease: "slow", duration: 2 })
 
+//////////////////////////////////////////////////////
+// SECTION 2
+//////////////////////////////////////////////////////
 
 let product = gsap.timeline({
   scrollTrigger: {
@@ -101,38 +110,102 @@ let product = gsap.timeline({
 })
 
 product
-  .from('.product', { opacity: 0 })
+  .from('.product', { opacity: 0, duration: 10 })
 
-  .from('.product-1', { opacity: 0, })
-  .to('.pic-1', { opacity: 1, rotation: 45 }, "<0")
-  .to('.product-1', { opacity: 0 })
-  .to('.pic-1', { opacity: 0 }, "<0")
-
-
-  .from('.product-2', { opacity: 0 })
-  .to('.pic-2', { opacity: 1, rotation: -45 }, "<0")
-  .to('.product-2', { opacity: 0 })
-  .to('.pic-2', { opacity: 0 }, "<0")
-
-  .from('.product-3', { opacity: 0 })
-  .to('.pic-3', { opacity: 1, rotation: 45 }, "<0")
-  .to('.product-3', { opacity: 0 })
-  .to('.pic-3', { opacity: 0 }, "<0")
-
-  .from('.product-4', { opacity: 0 })
-  .to('.pic-4', { opacity: 1, rotation: -45 }, "<0")
-  .to('.product-4', { opacity: 0 })
-  .to('.pic-4', { opacity: 0 }, "<0")
-
-  .from('.product-5', { opacity: 0 })
-  .to('.pic-5', { opacity: 1, rotation: 45 }, "<0")
-  .to('.product-5', { opacity: 0 })
-  .to('.pic-5', { opacity: 0 }, "<0")
-
-  .to('.product', { opacity: 0 })
+  .from('.product-1', { opacity: 0, ease: "slow", duration: 10, delay: 1 })
+  .to('.pic-1', { opacity: 1, rotation: 45, ease: "slow", duration: 1 }, "<0")
+  .to('.product-1', { opacity: 0, ease: "slow", duration: 10 }, "<10")
+  .to('.pic-1', { opacity: 0, ease: "slow", duration: 10 }, "<0")
 
 
+  .from('.product-2', { opacity: 0, ease: "slow", duration: 10, delay: 1 }, "<10")
+  .to('.pic-2', { opacity: 1, rotation: -45, ease: "slow", duration: 10 }, "<0")
+  .to('.product-2', { opacity: 0, ease: "slow", duration: 10 }, "<10")
+  .to('.pic-2', { opacity: 0, ease: "slow", duration: 10 }, "<0")
+
+  .from('.product-3', { opacity: 0, ease: "slow", duration: 10, delay: 1 }, "<10")
+  .to('.pic-3', { opacity: 1, rotation: 45, ease: "slow", duration: 10 }, "<0")
+  .to('.product-3', { opacity: 0, ease: "slow", duration: 10 }, "<10")
+  .to('.pic-3', { opacity: 0, ease: "slow", duration: 10 }, "<0")
+
+  .from('.product-4', { opacity: 0, ease: "slow", duration: 10, delay: 1 }, "<10")
+  .to('.pic-4', { opacity: 1, rotation: -45, ease: "slow", duration: 10 }, "<0")
+  .to('.product-4', { opacity: 0, ease: "slow", duration: 10 }, "<10")
+  .to('.pic-4', { opacity: 0, ease: "slow", duration: 10 }, "<0")
+
+  .from('.product-5', { opacity: 0, ease: "slow", duration: 10, delay: 1 }, "<10")
+  .to('.pic-5', { opacity: 1, rotation: 45, ease: "slow", duration: 10 }, "<0")
+  .to('.product-5', { opacity: 0, ease: "slow", duration: 10 }, "<10")
+  .to('.pic-5', { opacity: 0, ease: "slow", duration: 10 }, "<0")
+
+  .to('.product', { opacity: 0, duration: 10 })
 
 
+let gadgets2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".gadgets-2",
+    pin: true,
+    scrub: 1,
+  }
+})
 
+gadgets2
+  .to(".gadgets-2", { opacity: 1 })
+  .to(".mobile-2", { x: "-58vh", y: "-37%", scale: 0.55, opacity: 1 })
+  .to(".disap", { opacity: 0 }, "<0")
+  .to(".mobile-2", { opacity: 0 })
+  .to(".gadgets-2", { opacity: 0 }, "<0")
+
+
+let recweek = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".recweek-app",
+    pin: true,
+    scrub: 1,
+  }
+})
+
+recweek
+  .from(".recweek-app", { opacity: 0, duration: 1 })
+  .to(".recweek-app-pic", { x: "-200%" })
+  .to(".recweek-app", { opacity: 0, duration: 0.3 }, "<0")
+
+
+let qwiklabs = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".qwiklabs",
+    pin: true,
+    scrub: 1,
+  }
+})
+
+qwiklabs
+  .from(".qwiklabs", { opacity: 0, duration: 1 })
+  .from(".qwiklabs-pic", { x: "200%" })
+  .to(".qwiklabs-pic", { x: "200%" }, "<1")
+  .to(".qwiklabs", { opacity: 0, duration: 0.3 }, "<0")
+
+let bantayBayan = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".bantay-bayan",
+    pin: true,
+    scrub: 1,
+    markers: true,
+  }
+})
+
+bantayBayan
+  .from(".bantay-bayan", { opacity: 0, duration: 1 })
+  .from(".bantay-bayan-pic", { x: "-200%" })
+  .to(".bantay-bayan-pic", { x: "-200%" }, "<1")
+  .to(".bantay-bayan", { opacity: 0, duration: 0.3 }, "<0")
+
+let eventSlider = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".event-slider-pic",
+    pin: true,
+    scrub: 1,
+
+  }
+})
 
